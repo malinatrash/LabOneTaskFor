88e6d57676bce334b1b6d4b573e9d365363e4898
malinatrash <82772036+malinatrash@users.noreply.github.com>
2022-09-06 18:48:48 +0800

    Full application

---
+++ TaskFor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TaskFor
+{
+   public class Logic
+    {
+        public static int Calculate(int number)
+        {
+            int numberSquared = 0;
+            for (int iteration = 1; iteration <= number; iteration += 1)
+            {
+                numberSquared += (2 * iteration - 1);
+            }
+
+            return (numberSquared);
+        }
+    }
+    class Programm
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Please enter a number: ");
+            var number = int.Parse(Console.ReadLine());
+
+            var numberSquared = Logic.Calculate(number);
+
+            Console.WriteLine(numberSquared);
+        }
+    }
+}
---
+++ TaskForTests/LogicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskFor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFor.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void SquareIsEqualNine()
+        {
+            var number = 3;
+            var answer = Logic.Calculate(number);
+            Assert.AreEqual(9, answer);
+        }
+
+        [TestMethod()]
+        public void SquareIsEqualNull()
+        {
+            var number = 0;
+            var answer = Logic.Calculate(number);
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod()]
+        public void SquareIsEqualOne()
+        {
+            var number = 1;
+            var answer = Logic.Calculate(number);
+            Assert.AreEqual(1, answer);
+        }
+    }
+}
